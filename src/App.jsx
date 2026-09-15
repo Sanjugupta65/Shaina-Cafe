516d57ccaf2e1e52801d555c82dd4bbd553796e9
Sanju <sanju3139188@gmail.com>
2026-09-16 01:40:18 +0530

    fix page directing

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import StorySection from './components/StorySection'
+
+const App = () => {
+  return (
+    <div>
+      <Home/>
+      <Navbar/>
+      <StorySection/>
+    </div>
+  )
+}
+
+export default App
